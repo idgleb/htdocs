75051422f495fefbe30cf56525338ae8e0bdb8c0
idgleb2 <id_gleb@mail.ru>
2024-10-01 03:28:58 -0300

    3

--- clase1/funciones.php
+++ clase1/funciones.php
@@ -1,5 +1,12 @@
 <?php
 
+function test_input($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+  }
+
 function getMenu($categoria = null)
 {
     $lista_menu = [
