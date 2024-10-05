80c78a776fb8d878e86d620a60a0e4d268ec2002
idgleb2 <id_gleb@mail.ru>
2024-10-05 17:28:41 -0300

    jetinno

---
+++ jetinno/layout/footer.php
@@ -0,0 +1,18 @@
+<!-- footer.php -->
+<footer>
+    <div class="width90centr">
+        <div class="navabajo">
+            <a href="index.php">
+                <div class="logo"></div>
+            </a>
+            <div class="menu">
+                <a href="index.php">INICIO</a>
+                <a href="productos.php">PRODUCTOS</a>
+                <a href="tecnologia.php">TECNOLOGÍA</a>
+                <a href="contactos.php">CONTÁCTENOS</a>
+            </div>
+        </div>
+    </div>
+</footer>
+
+<?php include 'whatsapp.php'; ?>
---
+++ jetinno/productos.php
@@ -0,0 +1,123 @@
+<?php
+// Conexión a la base de datos
+$host = 'localhost';
+$user = 'root';
+$password = '';
+$dbname = 'jetinno_base_de_datos';
+
+// Crear conexión
+$conn = new mysqli($host, $user, $password, $dbname);
+
+// Verificar conexión
+if ($conn->connect_error) {
+    die("Conexión fallida: " . $conn->connect_error);
+}
+
+// Consulta para obtener los productos
+$sql = "SELECT id, nombre, img, caracteristicas FROM productos";
+$result = $conn->query($sql);
+
+?>
+
+
+
+
+
+<!DOCTYPE html>
+
+<?php $title = "PRODUCTOS"; ?>
+<?php include 'layout/metedatos_css.php'; ?>
+
+<body>
+    <?php include 'layout/header.php'; ?>
+
+    <main>
+        <div class="width90centr">
+            <h2>PRODUCTOS DESTACADOS</h2>
+        </div>
+
+        <?php
+        // Verificar si hay prod
+        if ($result->num_rows > 0) :
+            $iter = 0;
+            while ($row = $result->fetch_assoc()):
+                // Convertir las características de la cadena a un array
+                $caracteristicas = explode(',', $row['caracteristicas']);
+                $iter++;
+
+                if ($iter % 2 != 0): ?>
+                    <div class="col_row">
+                    <?php endif; ?>
+
+                    <div class="caja_producto">
+                        <div class="caja_prod_baton">
+                            <figure><img id="img_<?php echo $row['img']; ?>" src="img/<?php echo $row['img']; ?>.png" alt="<?php echo $row['img']; ?>"></figure>
+                            <a class="baton" href="#venta_<?php echo $row['img']; ?>">Ver más</a>
+                        </div>
+                        <div class="text_prod_cont">
+                            <h3><?php echo $row['nombre']; ?></h3>
+                            <ul>
+                                <?php foreach ($caracteristicas as $caracteristica): ?>
+                                    <li><?php echo trim($caracteristica); ?></li>
+                                <?php endforeach ?>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <?php if ($iter % 2 == 0 || $iter == $result->num_rows): ?>
+                    </div>
+                <?php endif; ?>
+
+
+
+                <div id="venta_<?php echo $row['img']; ?>">
+                <div class="modal_prod">
+                    <div class="caja_img_producto_mod">
+                        <img id="img_<?php echo $row['img']; ?>_mod" src="img/<?php echo $row['img']; ?>.png" alt="<?php echo $row['img']; ?>">
+                    </div>
+                    <div class="text_prod_cont">
+                        <h3><?php echo $row['nombre']; ?></h3>
+                        <ul>
+                            <?php foreach ($caracteristicas as $caracteristica): ?>
+                                <li><?php echo trim($caracteristica); ?></li>
+                            <?php endforeach ?>
+                        </ul>
+                    </div>
+                    <a href="#img_<?php echo $row['img']; ?>">X</a>
+                </div>
+            </div>
+
+
+
+            <?php
+            endwhile;
+        else : ?>
+            <h2>No se encontraron productos</h2>
+        <?php
+            
+        endif;
+        $conn->close();
+        ?>
+
+
+
+
+
+        <section>
+            <iframe width="760" height="415" src="https://www.youtube.com/embed/icu3Lys3NKU?si=RXNuVVFrBlUWnWpF"
+                title="YouTube video player"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        </section>
+
+
+
+    </main>
+
+
+    <?php include 'layout/footer.php'; ?>
+
+
+</body>
+
+</html>
